9af2684066cda66f7576835485854f1424470f45
Magne Normann <magnealvnor@gmail.com>
2014-02-28 15:12:45 +0100

    added main2

---
+++ src/main2.go
@@ -0,0 +1,34 @@
+package main
+
+import(
+   "fmt"
+   "elevTypes"
+   "messages"
+   "elevNet"
+   "comsManager"
+   "elevDrivers"
+   "elevOrders"
+   "elevCtrl"
+)
+
+type Elevator struct{
+	driver      Drivers_s
+	net         Net_s
+	coms        ComsManager_s
+	orders      Orders_s
+	fsm         Fsm_s
+}
+
+func main(){
+   var net = elevNet.init()
+   var coms = comsManager(net)
+   var drivers = elevDrivers.init()
+   var orders = elevOrders.init(driver)
+   var fsm = elevCtrl.init(driver, orders)
+   
+   var self = elevTypes.Elevator{net, coms, driver, orders, fsm}
+       
+
+   for{}
+    
+}
