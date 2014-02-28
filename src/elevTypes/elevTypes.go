fbc5131b59ac3bed60995929e1c6649bed2e25d6
Magne Normann <magnealvnor@gmail.com>
2014-02-28 15:04:42 +0100

    added types

---
+++ src/elevTypes/elevTypes.go
@@ -0,0 +1,45 @@
+package elevTypes
+
+const N_FLOORS = 4
+
+type Direction_t int 
+
+const (
+    UP Direction_t = iota
+    DOWN
+    NONE
+    N_DIR
+)
+
+type Button struct{
+	Floor int    
+	Dir Direction_t          
+}
+
+
+type Drivers_s struct{
+   ButtonChan chan Button
+	SensorChan chan int
+	MotorChan chan Direction_t
+	StopButtonChan chan bool
+	ObsChan chan bool
+}
+
+
+type Net_s struct{
+   Dummy int
+}
+
+type Order_t struct{
+   direction   Direction_t   
+   floor       int
+   status      bool
+}
+
+   
+   
+
+
+
+
+
