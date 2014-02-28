df6573b77e8f1c55e65a35d6b14cfdca948e5109
Magne Normann <magnealvnor@gmail.com>
2014-02-28 14:54:44 +0100

    added Orders

---
+++ src/elevOrders/elevOrders.go
@@ -0,0 +1,25 @@
+package elevOrders
+
+import(
+   "fmt"
+   "elevTypes"
+)
+
+type Orders_s struct{
+   table          [][]bool
+   ToFsm_new      chan elevTypes.Order_t  
+   ToComs_new     chan elevTypes.Order_t     
+   ToComs_del     chan elevTypes.Order_t
+}
+
+func Init() Orders_s{
+   fmt.Println("elevOrders.init()...")
+   
+   var table [][]bool
+   
+   toFsm_new   := make(chan elevTypes.Order_t)
+   toComs_new  := make(chan elevTypes.Order_t)
+   toFsm_del   := make(chan elevTypes.Order_t)
+   
+   return Orders_s{table, toFsm_new, toComs_new, toFsm_del}
+}
