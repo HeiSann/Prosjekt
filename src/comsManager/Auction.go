a98fda0aeb5396d7e453ed279f686ff1424ed1d3
FWorren <freworr@gmail.com>
2014-03-16 16:30:19 +0100

    changed auction time to 70

--- src/comsManager/Auction.go
+++ src/comsManager/Auction.go
@@ -8,7 +8,7 @@ import ("elevTypes"
 
 
 const SELECT_SLEEP_TIME = 2
-const AUCTION_DURATION = 30
+const AUCTION_DURATION = 70
 		
 
 func (self *ComsManager_s)getMyCost(order elevTypes.Order_t)int{ 
